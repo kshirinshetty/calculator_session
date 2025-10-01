5c9aa936645648bba0c1d0e0eb099bae4de72ef7
Kshirin Shetty <145857733+kshirinshetty@users.noreply.github.com>
2025-10-01 05:50:39 +0530

    Update test_calculator.py

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -12,14 +12,6 @@ class TestBasicOperations:
         assert add(2, 3) == 5
         assert add(10, 15) == 25
 
-    def test_add_negative_numbers():
-        assert add(-1, -1) == -2
-        assert add(-5, 3) == -2
-        
-    def test_subtract_negative_numbers():
-        assert subtract(-1, -1) == 0
-        assert subtract(-5, -3) == -2
-
     def test_subtract_positive_numbers(self):
         """Test subtracting positive numbers"""
         assert subtract(5, 3) == 2
